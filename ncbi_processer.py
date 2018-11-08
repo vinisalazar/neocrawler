678e8dc32cabd81ce7e0fc458c20ca945da8578f
Vini Salazar <viniws@gmail.com>
2018-11-08 18:45:51 -0200

    working on feature table function

--- ncbi_processer.py
+++ ncbi_processer.py
@@ -9,5 +9,19 @@ We want the sequence itself, the scientific name, and the tax id.
 handle = Entrez.efetch(db='nuccore', id='D50540', retmode='xml')
 record = Entrez.read(handle)
 
-print('\n'.join(f'{k}\t{v}' for k, v in record[0].items()))
-print('\n'.join(f'{k}\t{v}' for k, v in record[0]['GBSeq_feature-table'][0].items()))
+# This prints a summary of the information
+def summary(record):
+    print('\n'.join(f'{k}\t{v}' for k, v in record[0].items()))
+
+def feat_table(record):
+    feat_table = record[0]['GBSeq_feature-table'][0]["GBFeature_quals"]
+    for d in feat_table:
+        print('\n'.join(f'{k}\t{v}' for k, v in d.items()))
+    return feat_table
+
+
+table = feat_table(record)
+
+d = {}
+for i in table:
+    d[i["GBQualifier_name"]] = i["GBQualifier_value"]
