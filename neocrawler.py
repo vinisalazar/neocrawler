f145cc01f3fdba7cb4a4e614791aeab400b6cfff
Vini Salazar <viniws@gmail.com>
2018-11-08 17:51:47 -0200

    added docstring

--- neocrawler.py
+++ neocrawler.py
@@ -1,5 +1,9 @@
 import scrapy
 
+"""
+This file crawls the Bacterio 'names' pages. It should open the link for
+each Genus page, and then extract the accession number for each one.
+"""
 
 class EntrySpider(scrapy.Spider):
     name="entries"
@@ -10,10 +14,6 @@ class EntrySpider(scrapy.Spider):
         # 'http://www.bacterio.net/-allnamesmr.html',
         # 'http://www.bacterio.net/-allnamessz.html'
     ]
-
     def parse(self, response):
         for p in response.css('a[href*="https://www.ncbi.nlm.nih.gov/nuccore/"]::text'):
-            yield {
-                # 'genus': p.css('span.taxon-subhead-16s::text').extract_first()
-                'genus': p.extract()
-            }
+            yield { 'ncbi_id': p.extract() }
