227729f478876db04dea26c737c6610fb3789b1c
Vini Salazar <viniws@gmail.com>
2018-11-09 13:39:58 -0200

    added cleaner(file) function

--- ncbi_processer.py
+++ ncbi_processer.py
@@ -1,13 +1,11 @@
 from Bio import Entrez
-
 """
 This will process the output of our crawler with the Entrez module.
 We want the sequence itself, the scientific name, and the tax id.
 """
-
 # ID should be replaced for each
-handle = Entrez.efetch(db='nuccore', id='D50540', retmode='xml')
-record = Entrez.read(handle)
+#handle = Entrez.efetch(db='nuccore', id='D50540', retmode='xml')
+#record = Entrez.read(handle)
 
 # This prints a summary of the information
 def summary(record):
