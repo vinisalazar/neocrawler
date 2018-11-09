dac1b0baa1318e2ce6f986b9793a34e16f6cc014
Vini Salazar <viniws@gmail.com>
2018-11-09 11:35:57 -0200

    first working version

--- neocrawler.py
+++ neocrawler.py
@@ -1,20 +1,30 @@
 import scrapy
+from urllib.parse import urljoin
 
 """
 This file crawls the Bacterio 'names' pages. It should open the link for
 each Genus page, and then extract the accession number for each one.
 """
 
-class EntrySpider(scrapy.Spider):
-    name="entries"
+class BacteriumSpider(scrapy.Spider):
+    name="bacterium"
+
     start_urls = [
-        'http://www.bacterio.net/abiotrophia.html'
-        # 'http://www.bacterio.net/-allnamesac.html',
-        # 'http://www.bacterio.net/-allnamesdl.html',
-        # 'http://www.bacterio.net/-allnamesmr.html',
-        # 'http://www.bacterio.net/-allnamessz.html'
+        'http://www.bacterio.net/-allnamesac.html',
+        'http://www.bacterio.net/-allnamesdl.html',
+        'http://www.bacterio.net/-allnamesmr.html',
+        'http://www.bacterio.net/-allnamessz.html'
     ]
+
     def parse(self, response):
+        bacteria = response.css('a[href$=".html"]').extract()
+        bacteria = [b for b in bacteria if '¤' in b]
+        for b in bacteria:
+            b = b.split('"')[1]
+            url = urljoin(response.url, b)
+            yield scrapy.Request(url, callback=self.parse_bacteria)
+
+    def parse_bacteria(self, response):
         for p in response.css(
             'a[href*="https://www.ncbi.nlm.nih.gov/"]::text'
             ):
